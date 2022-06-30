cb6c80542cadd0d1b2125fd7c9c250beb2d0217c
nicktran69 <caonhantran1994@gmail.com>
2022-06-30 03:34:18 +0000

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres123456
+export POSTGRES_HOST=postgres-nhantc-dev.cmujl0ktskgc.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram_dev
+export AWS_BUCKET=arn:aws:s3:::s3nhantc
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
